13987d6c54b52ae4a56dfc109308454b0d21a883
Emilija <80635573+emillija@users.noreply.github.com>
2021-07-06 11:59:11 +0200

    Homework

--- Homework04/breakingRecord.js
+++ Homework04/breakingRecord.js
@@ -3,6 +3,7 @@
 let scores = [3, 4, 21, 36, 10, 28, 35, 5, 24, 42];
         // // 3  4  21  36  36  36  36 36  36  42 
 
+
 function breakingRecords(scores) {
     let minResult = scores[0];
     let maxResult = scores[0];
@@ -23,7 +24,6 @@ function breakingRecords(scores) {
     }
     return [maxRecord, minRecord];
 }; 
-
 
 const records = breakingRecords(scores);
 console.log(records);
